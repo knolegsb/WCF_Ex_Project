ac22839ee216762f83fed65e75ec4c7df474d63b
knolegsb <seon2you@hotmail.com>
2015-10-07 10:00:54 -0700

    start and evalservice

---
+++ EvalServiceLibrary/EvalService.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EvalServiceLibrary
+{
+    [ServiceBehavior(InstanceContextMode =InstanceContextMode.Single)]
+    public class EvalService : IEvalService
+    {
+        #region IEvalService Members
+        public List<Eval> GetEvals()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void RemoveEval(string id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SubmitEval(Eval eval)
+        {
+            throw new NotImplementedException();
+        }
+
+        #endregion
+    }
+}
