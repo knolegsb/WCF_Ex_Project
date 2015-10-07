fd7376881d7c56f8c7ba2f40eaab009b73f8f838
knolegsb <seon2you@hotmail.com>
2015-10-07 15:09:09 -0700

    service working well

---
+++ Client/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Client.EvalServiceReference;
+
+namespace Client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Press <Enter> to run the client...");
+            Console.ReadLine();
+
+            EvalServiceClient client = new EvalServiceClient("BasicHttpBinding_IEvalService");
+
+            Eval eval = new Eval();
+            eval.Comments = "This came from code!";
+            eval.Submitter = "Aaron";
+            eval.TimeSubmitted = DateTime.Now;
+
+            client.SubmitEval(eval);
+
+            Eval[] evals = client.GetEvals();
+            foreach (Eval ev in evals)
+                Console.WriteLine(ev.Comments);
+
+            Console.ReadLine();
+        }
+    }
+}
